b4bc665bbccff048626ef7d5cc10b2421c67ff89
Jennifer R <68330744+Jennifer-Rajkumar@users.noreply.github.com>
2022-03-13 12:37:24 +0530

    Add files via upload

---
+++ Day 5/JSON/home.js
@@ -0,0 +1,22 @@
+fetch('home.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+function appendData(data) {
+    var element = '';
+    for (var i = 0; i < data.length; i++) {
+        element += '<div class="card '+data[i].category.split(" ")[0]+'">';
+        element += '<div class="category">'+data[i].category+'</div>';
+        element += '<div class="description">'+data[i].description+'</div>';
+        element += '<div class="footer">';
+        element += '<div class="left"><i class="fa fa-envelope icon" aria-hidden="true"></i><i class="fa fa-paperclip icon" aria-hidden="true"></i></div>';
+        element += '<div class="right"><i class="fa fa-user icon" aria-hidden="true"></i></div></div></div>';
+    }
+    document.getElementById("myData").innerHTML = element;
+}
